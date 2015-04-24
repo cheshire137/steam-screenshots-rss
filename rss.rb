c9af8d46e0c643b2f24a48d8248aca335f66ff01
Sarah Vessels <cheshire137@gmail.com>
2015-04-24 11:05:18 -0400

    Require files, pass rss_url param

--- rss.rb
+++ rss.rb
@@ -2,11 +2,15 @@
 require 'rubygems'
 require 'mechanize'
 require 'rss'
+require_relative 'screenshot'
+require_relative 'steam_fetcher'
+require_relative 'steam_screenshot_rss'
 
 steam_user = 'cheshire137'
+rss_url = "#{steam_user}_steam_screenshots.rss"
 steam = SteamFetcher.new(steam_user)
 screenshots = steam.get_screenshots
-rss = SteamScreenshotRSS.new(steam_user).make_rss(screenshots)
+rss = SteamScreenshotRSS.new(steam_user, rss_url).make_rss(screenshots)
 
 print "Content-type: application/atom+xml\r\n\r\n"
 puts rss
