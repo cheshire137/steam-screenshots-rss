ac0e1291bc78592c04a58cb8e48090e9b8dbe643
Sarah Vessels <cheshire137@gmail.com>
2015-04-27 22:39:03 -0400

    Enable CORS

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'mechanize', '~> 2.7.3'
 gem 'sinatra', '~> 1.4.6'
+gem 'sinatra-cross_origin', '~> 0.3.2'
--- app.rb
+++ app.rb
@@ -3,6 +3,18 @@ require_relative 'screenshot'
 require_relative 'steam_fetcher'
 require_relative 'steam_screenshot_rss'
 
+configure do
+  enable :cross_origin
+end
+
+options '*' do
+  response.headers['Allow'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] =
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, ' +
+      'Accept'
+  200
+end
+
 get '/' do
   steam_user = params['user'] || 'cheshire137'
   steam = SteamFetcher.new(steam_user)
